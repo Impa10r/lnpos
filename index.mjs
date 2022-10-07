c65a1bd7acc517a79e1ff7a91035bb6c392de56c
impa10r <impalor@pm.me>
2022-10-07 10:49:26 +0200

    Fix http

--- index.mjs
+++ index.mjs
@@ -19,7 +19,7 @@ app
 
 if (process.env.NODE_ENV === 'prod') {
   // Secondary http app
-  var http = require('http');
+  import http from 'http';
   http.createServer((req, res) => {
     res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
     res.end();
