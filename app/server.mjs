166b173024c992642dd7d9d8376936a4bd58caed
impa10r <impalor@pm.me>
2022-10-10 23:55:19 +0200

    Fix robots.txt

--- app/server.mjs
+++ app/server.mjs
@@ -192,7 +192,7 @@ export default class Server {
 
     this.express.get('/robots.txt', (req, res) => {
       res.type('text/plain');
-      res.send('User-agent: *\nAllow: /$\nAllow: /?lang=es\nAllow: /?lang=ru\nDisallow: /');
+      res.send('User-agent: *\nAllow: /$\nDisallow: /\nAllow: /?lang=*');
     });
 
     this.express.get('/:id/:amount/:memo?', (req, res) => {
