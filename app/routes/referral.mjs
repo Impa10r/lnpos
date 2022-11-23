202deef728a52438c3d2328fc764ae64037c2997
impa10r <impalor@pm.me>
2022-11-24 00:41:17 +0100

    Fix affiliate lang

--- app/routes/referral.mjs
+++ app/routes/referral.mjs
@@ -3,12 +3,13 @@ import qr from 'qrcode';
 
 const router = express.Router();
 
-function renderError(req, res, errCode, err) {
+function renderError(req, res, errCode, err, link) {
   if (err) console.error(err);
   res.render('message', {
-    currentLocale: req.locale,
+    currentLocale: res.getLocale(),
     message: req.__(errCode),
     color: 'red',
+    link,
   });
 }
 
@@ -20,11 +21,11 @@ router.get('/affiliate', (req, res) => {
 });
 
 router.post('/referral', (req, res) => {
-  const currentLocale = res.getLocale();
+  const currentLocale = req.body.lang;
   const code = req.body.refCode;
   req.setLocale(currentLocale);
   if (code.length !== 10)
-    return renderError(req, res, 'error_invalid_ref');
+    return renderError(req, res, 'error_invalid_ref', '', '/affiliate?lang=' + currentLocale);
   const desc = req.get('host') + '/' + code;
   const url = req.protocol + '://' + desc;
   qr.toDataURL(url, (err, src) => {
